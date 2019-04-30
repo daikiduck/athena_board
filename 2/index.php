34d65b793f21dda204ffaea804cc90514e84d9a7
檜物大輝 <himonodaiki@himonodaikinoMacBook-Pro.local>
2019-04-30 14:14:11 +0900

    aaa

---
+++ 2/index.php
@@ -0,0 +1,12 @@
+<?php
+
+
+define("MY_EMAIL","daiki.himono@gmail.com");
+
+echo MY_EMAIL;
+
+
+var_dump(__LINE__);
+var_dump(__FILE__);
+var_dump(__DIR__);
+?>
---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+
+//初めてのPHP
+
+
+$msg="hello from the TOP! ";
+echo $msg;
+//echo "hello from the TOP! again! ";
+var_dump($msg)
+
+
+
+/*
+<!DOCTYPE html>
+<html lang="ja">
+<body>
+    <p>Hello World<?php echo " from PHP"; ?></p>
+</body>
+</html>
+*/
+
+?>
